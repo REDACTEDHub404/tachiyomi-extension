add5ed808f7eb0b2ecbc6f2b35b3dfa12b3a5234
Zhang <124061053+REDACTEDHub404@users.noreply.github.com>
2026-07-28 23:26:45 +0700

    Create BullyManga.kt

---
+++ src/th/bullymanga/src/eu/kanade/tachiyomi/extension/th/bullymanga/BullyManga.kt
@@ -0,0 +1,9 @@
+package eu.kanade.tachiyomi.extension.th.bullymanga
+
+import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+
+class BullyManga : MangaThemesia(
+    "Bully-Manga",
+    "https://bully-manga.com",
+    "th"
+)
